73748c6ed03d92d7b689dc5a165a184bc8f236aa
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2024-03-04 11:15:33 +0330

    Add BG and music

--- main.gd
+++ main.gd
@@ -10,15 +10,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
+	
 func game_over():
-		$ScoreTimer.stop()
-		$MobTimer.stop()
-		$HUD.show_game_over()
+	$Music.stop()
+	$GameOverSound.play()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
+	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
